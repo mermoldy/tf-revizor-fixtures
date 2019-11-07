29090ff213c7052ad6ef0735f211f5b8b30bece5
Olga Stasyuk <ostasyuk@users.noreply.github.com>
2019-11-07 10:14:18 +0200

    Update main.tf

--- azure/main.tf
+++ azure/main.tf
@@ -1,52 +1,30 @@
 provider "azurerm" {
-  client_id = "${var.scalr_azurerm_client_id}"
-  client_secret = "${var.scalr_azurerm_secret_key}"
-  environment = "${var.scalr_azurerm_client_id}"
-  subscription_id = "${var.scalr_azurerm_subscription_id}"
-  tenant_id = "${var.scalr_azurerm_tenant_id}"
-}
-
-resource "azurerm_resource_group" "main" {
-  name     = "${var.prefix}-resources"
-  location = "East US"
-}
-
-resource "azurerm_network" "main" {
-  name                = "${var.prefix}-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-}
-
-resource "azure_subnet" "internal" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_network.main.name
-  address_prefix       = "10.0.2.0/24"
+  client_id = var.scalr_azurerm_client_id
+  client_secret = var.scalr_azurerm_client_secret
+  subscription_id = var.scalr_azurerm_subscription_id
+  tenant_id = var.scalr_azurerm_tenant_id
+  environment =  var.scalr_azurerm_environment
 }
 
 resource "azurerm_network_interface" "main" {
-  name                = "${var.prefix}-network"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  name                = var.name
+  location            = var.region
+  resource_group_name = var.resource_group
 
   ip_configuration {
-    name                          = "testconfiguration1"
-    subnet_id                     = azurerm_subnet.internal.id
+    name                          = var.name
+    subnet_id                     = var.subnet_id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
-resource "azure_instance" "test_instance" {
-  name                  = "${var.prefix}-TF-vm"
-  location              = azurerm_resource_group.main.location
-  resource_group_name   = azurerm_resource_group.main.name
+resource "azurerm_virtual_machine" "web" {
+  location = var.region
+  name = var.name
   network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = var.vm_size
-
+  resource_group_name = var.resource_group
+  vm_size = var.instance_type
   delete_os_disk_on_termination = true
-
-  delete_data_disks_on_termination = true
 
   storage_image_reference {
     publisher = "Canonical"
@@ -54,19 +32,23 @@ resource "azure_instance" "test_instance" {
     sku       = "16.04-LTS"
     version   = "latest"
   }
+
   storage_os_disk {
     name              = "myosdisk1"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
+
   os_profile {
-    computer_name  = "hostname"
-    admin_username = "testadmin"
-    admin_password = "Password1234!"
+    admin_username = var.name
+    computer_name = var.name
+    admin_password = var.password
   }
+
   os_profile_linux_config {
     disable_password_authentication = false
   }
-  tags = "${var.prefix}-TF-vm"
+
+  tags = var.tags
 }
