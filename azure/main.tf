347e30b3459116d48775eb60af9864c49f754424
Olga Stasyuk <ostasyuk@users.noreply.github.com>
2019-11-15 13:39:40 +0200

    Update main.tf

--- azure/main.tf
+++ azure/main.tf
@@ -6,26 +6,10 @@ provider "azurerm" {
   environment =  var.scalr_azurerm_environment
 }
 
-resource "azurerm_network_interface" "main" {
-  name                = var.name
-  location            = var.region
-  resource_group_name = var.resource_group
-
-
-  ip_configuration {
-    name                          = var.name
-    subnet_id                     = var.subnet_id
-    private_ip_address_allocation = "Dynamic"
-  }
-  tags = {
-    owner = "revizor"  
-  }
-}
-
 resource "azurerm_virtual_machine" "Tf-test-instanse" {
   location = var.region
   name = var.name
-  network_interface_ids = [azurerm_network_interface.main.id]  
+  network_interface_ids = var.network
   
   resource_group_name = var.resource_group
   vm_size = var.instance_type
