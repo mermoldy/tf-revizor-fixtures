bcccf6d6eacd170fdcbfd12d91598ffd876112b9
gigimon <oleg@scalr.com>
2020-03-25 18:06:51 +0300

    * add random timeout (fix)

--- local_wait/main.tf
+++ local_wait/main.tf
@@ -12,7 +12,10 @@ resource "random_integer" "timeout" {
 }
 
 resource "null_resource" "wait" {
+  triggers = {
+    run_id = var.run_id
+  }
   provisioner "local-exec" {
-    command = "sleep ${random_integer.timeout.result}"
+    command = "sleep 30"
   }
 }
